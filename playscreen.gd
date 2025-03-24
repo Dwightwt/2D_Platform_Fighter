f8ff3db38dd4ccf6f6815694f8a6e3a9d0467953
Terron <dwightwt@gmail.com>
2025-03-24 14:35:00 -0500

    Create playscreen.gd

---
+++ playscreen.gd
@@ -0,0 +1,28 @@
+//Code for the playscreen to choose versus mode or challenge mode
+
+extends Control
+
+@onready var musicAudioStreamBG = $AudioStreamPlayerBGmusic2
+var backgroundsMusicOn = true
+
+func _process(delta):
+	update_music_stats()
+
+func update_music_stats():
+	if backgroundsMusicOn:
+		if !musicAudioStreamBG.playing:
+			musicAudioStreamBG.play()
+	else:
+		musicAudioStreamBG.stop()
+
+
+func _on_smash_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/StageSelection/stage_selection.tscn")
+
+
+func _on_challenges_pressed() -> void:
+	print("Challenges pressed")
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/Titlescreen/titlescreen.tscn")
