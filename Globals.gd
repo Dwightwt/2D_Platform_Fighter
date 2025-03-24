8f3d8e9c2b2939ffb260a3a020b79589e38de4f1
Terron <dwightwt@gmail.com>
2025-03-24 13:34:54 -0500

    Create Globals.gd

---
+++ Globals.gd
@@ -0,0 +1,9 @@
+//Global Function for Hitstun (Make available for all scripts to access)
+
+extends Node
+
+func hitstun(mod, duration):
+	Engine.time_scale = mod/100
+	print(str(mod))
+	await get_tree().create_timer(duration * Engine.time_scale).timeout
+	Engine.time_scale = 1
