0d6b5bb1de2010e5d48254e8ddf4a516541b6cf8
Terron <dwightwt@gmail.com>
2025-03-24 18:28:17 -0500

    Create walk_state.gd

---
+++ walk_state.gd
@@ -0,0 +1,25 @@
+//Walk state for enemy AI
+
+extends NodeState
+
+@export var character_body_2d : CharacterBody2D
+@export var animated_sprite_2d : AnimatedSprite2D
+@export var speed : int = 50
+
+func on_process(delta: float):
+	pass
+
+
+func on_physics_process(delta: float):
+	character_body_2d.velocity.x = move_toward(character_body_2d.velocity.x, 0, speed * delta)
+	animated_sprite_2d.play("walk")
+	character_body_2d.move_and_slide()
+
+
+func enter():
+	pass
+
+
+
+func exit():
+	pass
